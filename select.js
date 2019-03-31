fad41d73564f6f60e10f50d5167f3b2c34c6d6a0
mkrajnak <mkrajnak>
2019-03-31 03:17:50 +0200

    Add files

---
+++ select.js
@@ -0,0 +1,20 @@
+
+document.getElementById('body').onload = () => {
+  console.log("Hello");
+  var select = document.getElementById("custom-select");
+  console.log(document.getElementById("custom-select"));
+
+}
+console.log("Hello2");
+
+
+
+// console.log(select.options)
+
+// select.options[select.options.length] = new Option('Lel', 'lel');
+
+// class Extend Evented {
+//   constructor() {
+//     super()
+//   }
+// }
