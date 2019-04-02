9a5f6927e9fc2679bd2da3db8daafd9f83a6d707
mkrajnak <mkrajnak>
2019-04-02 23:19:35 +0200

    Checkpoint

--- select.js
+++ select.js
@@ -1,10 +1,11 @@
+// Open/Close menu
 var changeState = (select) => {
   for (opt of select.getElementsByTagName('opt')) {
     opt.style.display = opt.style.display === 'none' ? 'block' : 'none';
   }
 }
 
-
+// Click outside => close all customselect elements
 var closeAll = () => {
   nodes = Array.from(document.getElementsByTagName("customselect"));
   nodes.forEach((select) => {
@@ -15,26 +16,33 @@ var closeAll = () => {
 }
 
 
-var filterItems = (e, label) => {
-  nodes = Array.from(document.getElementsByTagName("customselect"));
-  nodes.forEach((select) => {
-    for (opt of select.getElementsByTagName('opt')) {
-      if (!opt.getElementsByTagName('value')[0].innerHTML.includes(label.value)) {
-        opt.style.display = 'none';
-      }
+var filterSelect = (select, filter) => {
+  for (opt of select.getElementsByTagName('opt')) {
+    if (!opt.getElementsByTagName('value')[0].innerHTML.includes(filter)) {
+      opt.style.display = 'none';
     }
-  });
+  }
 }
 
+var filterEvent = (e, label) => {
+  closeAll();
+  changeState(label.parentNode);
+  filterSelect(label.parentNode, label.value)
+}
 
+// clicked option => value in label is changed + menu is closed
 var changeValue = (opt) => {
   let select = opt.parentNode.parentNode
   let changed = opt.getElementsByTagName('value')[0].innerHTML;
-  select.getElementsByTagName('xlabel')[0].innerHTML = changed;
-  changeState(select);
+  if (select.mode === '1') {
+    select.getElementsByTagName('xlabel')[0].innerHTML = changed;
+  } else {
+    select.getElementsByTagName('input')[0].value = changed;
+  }
+  closeAll();
 }
 
-
+// Generate option tags
 var createOption = (opt) => {
   let customOpt = document.createElement("opt");
   let value = document.createElement("value");
@@ -50,42 +58,49 @@ var createOption = (opt) => {
   return customOpt;
 }
 
+// Handle all click events in window
 var clickHandler = (target) => {
+  closeAll()
   if (target.tagName === 'XLABEL') {
     changeState(target.parentNode);
+  } else if (target.tagName === 'INPUT') {
+    changeState(target.parentNode);
+    filterSelect(target.parentNode, target.value);
   } else if (target.tagName === 'OPT') {
     changeValue(target)
   } else if (target.tagName === 'VALUE' || target.tagName === 'DESCRIPTION') {
     changeValue(target.parentNode)
-  } else {
-    closeAll()
   }
 }
 
 
+var getCustomLabel = (mode) => {
+  if (mode === '1') {
+    return document.createElement("xlabel");
+  } else {
+    let lbl = document.createElement("input");
+    lbl.placeholder = 'Type to search';
+    return lbl;
+  }
+}
+
 var createCustomSelectElement = (select) => {
-  console.log(select)
-  // Get rid of the old select
-  const options = select.options
-  select.parentNode.removeChild(select)
-  // Add new select
+  // Read all options, get rid of the old select
+  const options = select.options;
+  select.parentNode.removeChild(select);
+  // Add new select, set mode, new options
   var customSelect = document.createElement("customselect");
+  customSelect.mode = select.getAttribute('mode');
   var customOptions = document.createElement("options");
-
-
-  if (select.getAttribute('mode') === '1') {
-    var customLabel = document.createElement("xlabel");
-   } else {
-     var customLabel = document.createElement("input");
-   }
+  var customLabel = getCustomLabel(customSelect.mode);
 
   var id = document.createAttribute("id");
-  id.value = select.id
+  id.value = select.id;
   customSelect.setAttributeNode(id);
   // Create options and add first value
   for (opt of options) {
-    if (!customLabel.innerHTML && select.getAttribute('mode') === '1') {
-      customLabel.innerHTML = opt.text ;
+    if (!customLabel.innerHTML && customSelect.mode === '1') {
+      customLabel.innerHTML = opt.text;
     }
     customOptions.appendChild(createOption(opt));
   }
@@ -93,14 +108,14 @@ var createCustomSelectElement = (select) => {
   var arrow = document.createElement("options");
   customSelect.appendChild(customLabel);
   customSelect.appendChild(customOptions);
-  document.body.append(customSelect)
+  document.body.append(customSelect);
   // Register events
-  if (select.getAttribute('mode') === '2') {
-    customLabel.addEventListener('keypress', (e) => filterItems(e, customLabel))
+  if (customSelect.mode === '2') {
+    customLabel.addEventListener('input', (e) => filterEvent(e, customLabel));
   }
 }
 
-
+// Kick off once the document is loaded
 var loaded = () => {
   window.removeEventListener('load', loaded);
   nodes = Array.from(document.getElementsByClassName("custom-select"));
